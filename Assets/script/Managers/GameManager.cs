78d3f2d4fd094fb60619a427eb9f45e3aea4fc99
lukep__ <lucasprado2006@gmail.com>
2025-11-06 19:35:10 -0300

    .

--- Assets/script/Managers/GameManager.cs
+++ Assets/script/Managers/GameManager.cs
@@ -15,36 +15,19 @@ public class GameManager : MonoBehaviour
     [Header("Submarino")]
     public Submarino submarinoData;
     
-    [Header("UI/Dados")]
+    [Header("Recursos jogador")]
 
     public int QtdFerro;
     public float QtdComb;
     [SerializeField] private float decrementoComb;
     public bool gameover;
-    [Header("Waves")]
-    public WaveSpawner waveSpawner;
-    public float timeBtwWaves;
-
-    private int currentWave = 0;
-    public event Action AllWavesCompleted;
-    public BossController bigBoss;
-
-    public RadarWave radarWaveUi;
-
-    [SerializeField] private GameObject botaoModoTiro;
-
-
-    [Header("Dados do caminho")]
-    public Slider progressSlider;
-
-    private int totalEtapas;
-    private int etapasCompletas;
-
+    
     public event Action OnTileStateChanged;
 
     [Header("SubManagers")]
     public BuildManager buildManager;
     public UIManager uiManager;
+    public WaveManager waveManager;
 
 
     void Awake()
@@ -57,65 +40,18 @@ public class GameManager : MonoBehaviour
         
         if (SceneManager.GetActiveScene().name == "MainScene")
         {
-            if (waveSpawner == null)
+
+            if(waveManager == null)
             {
-                waveSpawner = FindFirstObjectByType<WaveSpawner>();
+                FindFirstObjectByType<WaveManager>();
             }
-
-            if (bigBoss == null)
-            {
-                bigBoss = FindFirstObjectByType<BossController>();
-            }
-
-            
-            totalEtapas = waveSpawner.waves.Length + 1;
-            progressSlider.minValue = 0f;
-            progressSlider.maxValue = 1f;
-            progressSlider.value = 0f;
-            
-
-            waveSpawner.OnWaveCompleted += OnWaveCompleta;
-            bigBoss.OnBossDefeated += OnBossDerrotado;
-
-
-            StartCoroutine(StartWaves());
-            AllWavesCompleted += () =>
-            {
-                bigBoss.StartBossFight();
-                botaoModoTiro.SetActive(true);
-            }; 
             
         }
 
     }
-
-    IEnumerator StartWaves()
-    {
-        
-
-        for (int i = 0; i < waveSpawner.waves.Length; i++)
-        {
-            radarWaveUi.AtualizarRadar(waveSpawner.waves[i]);
-            bool waveTerminou = false;
-            Action waveHandler = () => waveTerminou = true;
-
-            waveSpawner.OnWaveCompleted += waveHandler;
-            waveSpawner.StartWave(i);
-
-            yield return new WaitUntil(() => waveTerminou);
-            waveSpawner.OnWaveCompleted -= waveHandler;
-
-            yield return new WaitForSeconds(timeBtwWaves);
-        }
-
-        Debug.Log("Waves finalizadas, começando boss");
-        AllWavesCompleted?.Invoke();
-    }
-
-
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F5))
+        if (Input.GetKeyDown(KeyCode.F5))
         {
             QtdComb += 1000;
             QtdFerro += 1000;
@@ -123,14 +59,7 @@ public class GameManager : MonoBehaviour
         }
         if (submarinoData != null)
         {
-            if (QtdComb >= 0)
-            {
-                QtdComb -= decrementoComb * Time.deltaTime;
-            }
-            else
-            {
-                QtdComb = 0;
-            }
+            AtualizarCombustível();
 
             uiManager.AtualizarRecursosHUD();
 
@@ -158,21 +87,6 @@ public class GameManager : MonoBehaviour
             }
 
         }
-
-        if (waveSpawner != null && currentWave >= waveSpawner.waves.Length)
-        {
-            Time.timeScale = 0f;
-            uiManager.MostrarWin(true);
-            buildManager.customCursor.gameObject.SetActive(false);
-            Cursor.visible = true;
-            if (submarinoData != null)
-            {
-                Destroy(submarinoData.gameObject);
-            }
-
-        }
-        
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (uiManager.isPaused)
@@ -180,6 +94,22 @@ public class GameManager : MonoBehaviour
             else
                 uiManager.AlternarPause();
         }
+    }
+    
+
+    private void AtualizarCombustível()
+    {
+        if (QtdComb > 0)
+        {
+            QtdComb -= decrementoComb * Time.deltaTime;
+
+        }
+        else
+        {
+            QtdComb = 0;
+        }
+
+        uiManager.AtualizarRecursosHUD();
     }
 
     public void BuyBuilding(Building building)
@@ -241,26 +171,6 @@ public class GameManager : MonoBehaviour
     {
         Application.Quit();
     }
-
-    private void OnWaveCompleta()
-    {
-        etapasCompletas++;
-        AtualizarSlider();
-    }
-
-    private void OnBossDerrotado()
-    {
-        etapasCompletas++;
-        AtualizarSlider();
-        botaoModoTiro.SetActive(false);
-    }
-
-    private void AtualizarSlider()
-    {
-        float progresso = (float)etapasCompletas / totalEtapas;
-        progressSlider.value = progresso;
-    }
-
     public void AlternarModoTiro()
     {
         foreach (var torretas in TorretaBasica.TodasTorretas)
---
+++ Assets/script/Managers/Wave Manager.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveManager : MonoBehaviour
+{
+    [Header("Referencias")]
+    public WaveSpawner waveSpawner;
+    public BossController bigBoss;
+    public RadarWave radarWaveUi;
+    [SerializeField] private GameObject botaoModoTiro;
+    public Slider progressSlider;
+
+    [Header("Configs")]
+    public float timeBtwWaves;
+    public bool wavesComecaram;
+    private int currentWave = 0;
+    private int totalEtapas;
+    private int etapasCompletas;
+
+
+    public event Action AllWavesCompleted;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+                    if (waveSpawner == null)
+            {
+                waveSpawner = FindFirstObjectByType<WaveSpawner>();
+            }
+
+            if (bigBoss == null)
+            {
+                bigBoss = FindFirstObjectByType<BossController>();
+            }
+
+            
+            totalEtapas = waveSpawner.waves.Length + 1;
+            progressSlider.minValue = 0f;
+            progressSlider.maxValue = 1f;
+            progressSlider.value = 0f;
+            
+
+            waveSpawner.OnWaveCompleted += OnWaveCompleta;
+            bigBoss.OnBossDefeated += OnBossDerrotado;
+
+
+            StartCoroutine(StartWaves());
+    }
+
+    IEnumerator StartWaves()
+    {
+        wavesComecaram = true;
+        for (int i = 0; i < waveSpawner.waves.Length; i++)
+        {
+            radarWaveUi?.AtualizarRadar(waveSpawner.waves[i]);
+            bool waveTerminou = false;
+            Action waveHandler = () => waveTerminou = true;
+
+            waveSpawner.OnWaveCompleted += waveHandler;
+            waveSpawner.StartWave(i);
+
+            yield return new WaitUntil(() => waveTerminou);
+            waveSpawner.OnWaveCompleted -= waveHandler;
+
+            yield return new WaitForSeconds(timeBtwWaves);
+        }
+
+        Debug.Log("Waves finalizadas, começando boss");
+        AllWavesCompleted?.Invoke();
+
+
+        bigBoss?.StartBossFight();
+        botaoModoTiro.SetActive(true);
+        
+    }
+
+    private void OnWaveCompleta()
+    {
+        etapasCompletas++;
+        AtualizarSlider();
+    }
+
+    private void OnBossDerrotado()
+    {
+        etapasCompletas++;
+        AtualizarSlider();
+        botaoModoTiro.SetActive(false);
+
+        GameManager.Instance.uiManager.MostrarWin(true);
+        Time.timeScale = 0f;
+    }
+
+    private void AtualizarSlider()
+    {
+        if (progressSlider == null) return;
+        float progresso = (float)etapasCompletas / totalEtapas;
+        progressSlider.value = progresso;
+    }
+    
+    public bool TodasWavesConcluidas()
+    {
+        return wavesComecaram && currentWave >= waveSpawner.waves.Length;
+    }
+}
